5061a84190c1411daf5a8490183fd794a0643fbf
shweta-singh22 <rajputshweta2216@gmail.com>
2026-06-07 09:07:37 +0530

    Day-7 uploaded.

---
+++ Day-7/Que28.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int reverse(int n, int rev) {
+    if(n == 0)
+        return rev;
+    else
+        return reverse(n / 10, rev * 10 + (n % 10));
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Reversed number: %d", reverse(num, 0));
+
+    return 0;
+}
