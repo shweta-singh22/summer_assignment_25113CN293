a730984b4a55c3b16d274672404a4b373463275b
shweta-singh22 <rajputshweta2216@gmail.com>
2026-06-11 22:29:51 +0530

    Day-11 uploaded.

---
+++ Day-11/Que42.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    if(a > b)
+        return a;
+    else
+        return b;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Maximum = %d", max(num1, num2));
+
+    return 0;
+}
---
+++ Day-11/Que44.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial = %d", factorial(num));
+
+    return 0;
+}
