5201533caf6789ccc92228be0443429d05e9e3ef
shweta-singh22 <rajputshweta2216@gmail.com>
2026-06-13 12:09:49 +0530

    Day13.c uploaded.

---
+++ Day13.c/Que50.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n;
+    int sum = 0;
+    float avg;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    avg = (float)sum / n;
+
+    printf("Sum = %d\n", sum);
+    printf("Average = %.2f", avg);
+
+    return 0;
+}
